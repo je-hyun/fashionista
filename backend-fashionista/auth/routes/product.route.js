df0e7221cddb7452e6aa244802b972603edb7016
Je Hyun Kim <jhynkm@gmail.com>
2020-11-17 22:48:56 -0500

    Add manage products route and edit product route

--- backend-fashionista/auth/routes/product.route.js
+++ backend-fashionista/auth/routes/product.route.js
@@ -8,7 +8,7 @@ module.exports = function (app) {
     app.get('/api/products', products.findAll);
 
     // Find a single Product by Id
-    app.get('/api/productbyid/:id', products.findProductById);
+    app.get('/api/products/:id', products.findProductById);
 
     // Find a single Product by Name
     app.get('/api/products/:productName', products.findByName);
